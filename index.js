f5a5e2ce2da8a65b9cbfc867648c3f6b012467ff
unknown <framin85@gmail.com>
2022-05-11 18:39:46 +0300

    110522

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const Telegram = require('node-telegram-bot-api');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const token = process.env.token;
-console.log(1)
+console.log(token)
 const bot = new Telegram(token, {polling: true});
-bot.on()
-
+bot.onText(/\/start/,(msg)=>{
+    welcomeMessage(msg.from.id,'lalala')
+})
+function welcomeMessage (adressat,text){
+    bot.sendMessage(adressat, text)
+}
